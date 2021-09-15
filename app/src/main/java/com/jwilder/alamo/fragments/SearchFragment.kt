42c2194fbce71c52d708aaaae545645795c6c8a4
Scott Phree <refactored.spoon@pm.me>
2021-09-15 14:47:00 -0400

    hilt fix

--- app/src/main/java/com/jwilder/alamo/fragments/SearchFragment.kt
+++ app/src/main/java/com/jwilder/alamo/fragments/SearchFragment.kt
@@ -33,7 +33,7 @@ class SearchFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         binding.buttonFirst.setOnClickListener {
-            findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
+            findNavController().navigate(R.id.action_SearchFragment_to_MapFragment)
         }
     }
 
