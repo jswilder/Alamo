ffa5772e85ab72c057b1421bd790b7ec66db9f31
Scott Phree <refactored.spoon@pm.me>
2021-09-15 21:11:23 -0400

    simple RV and search

--- app/src/main/java/com/jwilder/alamo/fragment/SearchFragment.kt
+++ app/src/main/java/com/jwilder/alamo/fragment/SearchFragment.kt
@@ -6,6 +6,10 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.Observer
+import androidx.navigation.fragment.findNavController
+import com.jwilder.alamo.R
+import com.jwilder.alamo.VenueAdapter
 import com.jwilder.alamo.databinding.FragmentSearchBinding
 import com.jwilder.alamo.viewmodel.PlacesViewModel
 import dagger.hilt.android.AndroidEntryPoint
@@ -17,8 +21,6 @@ import dagger.hilt.android.AndroidEntryPoint
 class SearchFragment : Fragment() {
 
     private var _binding: FragmentSearchBinding? = null
-
-    // This property is only valid between onCreateView and onDestroyView
     private val binding get() = _binding!!
 
     val viewModel: PlacesViewModel by viewModels()
@@ -28,15 +30,23 @@ class SearchFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         _binding = FragmentSearchBinding.inflate(inflater, container, false)
+
+        viewModel.venueList.observe(viewLifecycleOwner, Observer { list ->
+            binding.venuesRecyclerView.adapter = VenueAdapter(list)
+        })
+
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        binding.buttonFirst.setOnClickListener {
-//            findNavController().navigate(R.id.action_SearchFragment_to_MapFragment)
-            viewModel.fetchNearbyPlaces("coffee")
+        binding.searchView.setOnSearchClickListener {
+            viewModel.fetchNearbyPlaces(binding.searchView.query.toString())
+        }
+
+        binding.mapsFAB.setOnClickListener {
+            findNavController().navigate(R.id.action_SearchFragment_to_MapFragment)
         }
     }
 
--- app/src/main/java/com/jwilder/alamo/viewmodel/PlacesViewModel.kt
+++ app/src/main/java/com/jwilder/alamo/viewmodel/PlacesViewModel.kt
@@ -1,7 +1,10 @@
 package com.jwilder.alamo.viewmodel
 
 import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.jwilder.alamo.remote.Venue
 import com.jwilder.alamo.repository.PlacesRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.CoroutineScope
@@ -21,6 +24,10 @@ class PlacesViewModel @Inject constructor(
         get() = job + Dispatchers.Default
     private val scope = CoroutineScope(coroutineContext)
 
+    val venueList: LiveData<List<Venue>>
+        get() = _venueList
+    private val _venueList = MutableLiveData<List<Venue>>()
+
     fun fetchNearbyPlaces(searchTerm: String) {
         scope.launch {
             try {
@@ -33,9 +40,15 @@ class PlacesViewModel @Inject constructor(
                     20180323
                 )
                 if (response.isSuccessful) {
-                    Log.d(TAG, response.body().toString())
+                    response.body()?.response?.venues?.let {
+                        _venueList.postValue(it)
+                        Log.d(TAG, it.toString())
+                    } ?: run {
+                        // TODO: Error state
+                        Log.d(TAG, "Venues list is null")
+                    }
                 } else {
-                    Log.d(TAG, "Failed")
+                    Log.d(TAG, "Response Failed")
                 }
             } catch (e: Exception) {
                 Log.d(TAG, e.message.toString())
